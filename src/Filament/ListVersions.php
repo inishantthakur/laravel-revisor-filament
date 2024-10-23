57d301eddba263b58c7ea4dd42fa03fa9115f53f
Shea <shea@livesource.co.nz>
2024-10-23 17:03:00 +1300

    wip

--- src/Filament/ListVersions.php
+++ src/Filament/ListVersions.php
@@ -2,14 +2,18 @@
 
 namespace Indra\RevisorFilament\Filament;
 
+use Filament\Forms\Form;
 use Filament\Resources\Pages\Concerns\InteractsWithRecord;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Tables\Actions\ActionGroup;
 use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Indra\Revisor\Contracts\HasRevisor;
 
 class ListVersions extends ListRecords
 {
@@ -38,34 +42,39 @@ class ListVersions extends ListRecords
 
         return $table
             ->columns([
-                TextColumn::make('version_number')->label('Version #'),
+                TextColumn::make('version_number')
+                    ->label('Version #'),
                 TextColumn::make('title'),
-                IconColumn::make('is_current')->boolean(),
-                IconColumn::make('is_published')->boolean(),
+                IconColumn::make('is_current')
+                    ->label('Current')
+                    ->boolean(),
+                IconColumn::make('is_published')
+                    ->label('Published')
+                    ->boolean(),
             ])
             ->actions([
                 ActionGroup::make([
                     ViewVersionTableAction::make(),
+                    EditAction::make()
+                        ->hidden(fn (Model&HasRevisor $record) => ! $record->is_current),
                     RestoreTableAction::make(),
-                    //->record(fn (Model & HasRevisor $record) => $record)
-                    //->hidden(fn (Model & HasRevisor $record) => $record->is_current),
-                    //                    EditAction::make()->record(fn (Model & HasRevisor $record) => $record->draftRecord)
-                    //                        ->hidden(fn (Model & HasRevisor $record) => ! $record->is_current),
-                    //                    Action::make('view')
-                    //                        ->label(fn (Model & HasRevisor $record) => $record->is_current ? 'Edit' : 'View')
-                    //                        ->icon(fn (Model & HasRevisor $record) => $record->is_current ? 'heroicon-o-pencil' : 'heroicon-o-eye')
-                    //                        ->url(
-                    //                            fn (Model & HasRevisor $record) => $record->is_current ?
-                    //                                static::getUrl('edit', ['record' => $record->record_id]) :
-                    //                                static::getUrl('version', [
-                    //                                    'record' => $record->record_id,
-                    //                                    'version' => $record->id,
-                    //                                ])
-                    //                        ),
                     DeleteAction::make(),
                 ]),
             ])->modifyQueryUsing(function (Builder $query) use ($parent): Builder {
                 return $query->withVersionContext()->where('record_id', $parent);
             });
+    }
+
+    protected function configureEditAction(EditAction $action): void
+    {
+        $resource = static::getResource();
+
+        $action
+            ->authorize(fn (Model $record): bool => $resource::canEdit($record->draftRecord))
+            ->form(fn (Form $form): Form => $this->form($form->columns(2)));
+
+        if ($resource::hasPage('edit')) {
+            $action->url(fn (Model $record): string => $resource::getUrl('edit', ['record' => $record->record_id]));
+        }
     }
 }
