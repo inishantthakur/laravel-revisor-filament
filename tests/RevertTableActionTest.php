07414c788c81c2c9545bd2326298e1ea2a11684d
Shea <shea@livesource.co.nz>
2024-10-25 10:55:30 +1300

    finish tests/RevertTableActionTest.php

--- tests/RevertTableActionTest.php
+++ tests/RevertTableActionTest.php
@@ -1,8 +1,11 @@
 <?php
 
+use Indra\RevisorFilament\Filament\RevertTableAction;
 use Indra\RevisorFilament\Tests\Models\Page;
+use Indra\RevisorFilament\Tests\Resources\PageResource\Pages\ListPageVersions;
+use function Pest\Livewire\livewire;
 
-it('only shows on versions that are not current', function () {
+it('only shows on versions that are not current and can be reverted', function () {
     $page = Page::create(['title' => 'page'])->publish();
     $page->refresh();
     $version1 = $page->currentVersionRecord;
@@ -10,9 +13,10 @@ it('only shows on versions that are not current', function () {
     $page->update(['title' => 'updated page']);
     $version2 = $page->currentVersionRecord;
 
-    // todo - this is not working in the test suite
-    // fine in the browser...
-    //    livewire(ListPageVersions::class, ['record' => $page->id])
-    //        ->assertTableActionHidden(RevertTableAction::class, record: $version2)
-    //        ->assertTableActionVisible(RevertTableAction::class, record: $version1);
+    livewire(ListPageVersions::class, ['record' => $page->id])
+        ->assertTableActionHidden(RevertTableAction::class, $version2)
+        ->assertTableActionVisible(RevertTableAction::class, $version1->refresh())
+        ->callTableAction('revert', $version1);
+
+    $this->expect($page->refresh()->title)->toBe('page');
 });
